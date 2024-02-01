cea64df4917c8b5f33d96ef528d8d0a171f42f51
Ivan Pritychenko <5909281+Discoverivan@users.noreply.github.com>
2024-02-01 15:51:11 +0300

    Fix accept header (#6)

--- sample-app/src/main/kotlin/com/alfa/api/sdk/sample/app/configuration/GlobalExceptionHandler.kt
+++ sample-app/src/main/kotlin/com/alfa/api/sdk/sample/app/configuration/GlobalExceptionHandler.kt
@@ -1,5 +1,7 @@
 package com.alfa.api.sdk.sample.app.configuration
 
+import com.alfa.api.sdk.client.exceptions.ApiException
+import com.alfa.api.sdk.common.exceptions.SdkException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -13,7 +15,15 @@ class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     fun handleAnyException(e: Exception): String {
-        log.error("Got an error", e)
+        if (e is SdkException && e.cause != null && e.cause is ApiException) {
+            val apiException = e.cause as ApiException
+            log.error(
+                "Got an SDK Error with ApiException. Code: {}, response: {}",
+                apiException.statusCode, String(apiException.response), e
+            )
+        } else {
+            log.error("Got an error", e)
+        }
         return "Got an error: " + e.message
     }
 
