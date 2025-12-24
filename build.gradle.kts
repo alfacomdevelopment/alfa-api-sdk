bd1e72945f955e47a794f99fb8e7392c3100328e
Ivan Pritychenko <5909281+Discoverivan@users.noreply.github.com>
2025-12-24 18:28:32 +0300

    chore: added versioning plugin (#20)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,23 @@
+plugins {
+    id("pl.allegro.tech.build.axion-release") version "1.21.1"
+}
+
 allprojects {
     group = "com.alfa.api.sdk"
+    project.version = rootProject.version
 
     repositories {
         mavenCentral()
     }
 }
+
+scmVersion {
+    tag {
+        prefix = "v"
+    }
+    releaseOnlyOnReleaseBranches = true
+    releaseBranchNames = listOf("main")
+}
+
+
+version = scmVersion.version
